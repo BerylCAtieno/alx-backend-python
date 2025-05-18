59073b888fad4df031c0e3b863b1f1d2d29e3c55
berylatieno <berylatieno30@gmail.com>
2025-05-18 20:38:58 +0300

    pagination

---
+++ python-generators-0x00/2-lazy_paginate.py
@@ -0,0 +1,25 @@
+import mysql.connector
+
+def paginate_users(page_size, offset):
+    connection = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='',  # set your password if needed
+        database='ALX_prodev'
+    )
+    cursor = connection.cursor(dictionary=True)
+    query = "SELECT * FROM user_data LIMIT %s OFFSET %s"
+    cursor.execute(query, (page_size, offset))
+    results = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return results
+
+def lazy_paginate(page_size):
+    offset = 0
+    while True:
+        page = paginate_users(page_size, offset)
+        if not page:
+            break
+        yield page
+        offset += page_size
